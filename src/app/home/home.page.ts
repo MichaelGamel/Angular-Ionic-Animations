dc10b56baff5385830e51fa851604e4181c1491b
Michael <michael.gamel@hotmail.com>
2019-09-29 15:29:03 +0400

    Fade In Animation for Ionic Modal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { DetailPage } from '../detail/detail.component';
+import { ModalController } from '@ionic/angular';
+import { myFadeInAnimation } from '../animations/fade-in';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  public cards = new Array(10);
 
-  constructor() {}
+  constructor(private modalCtrl: ModalController) {}
 
+  launchDetail(ev) {
+    this.modalCtrl
+      .create({
+        component: DetailPage,
+        enterAnimation: myFadeInAnimation,
+        componentProps: {
+          coords: {
+            x: ev.target.x,
+            y: ev.target.y
+          }
+        }
+      })
+      .then(modal => {
+        modal.present();
+      });
+  }
 }
